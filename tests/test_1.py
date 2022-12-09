f0ecfd9ee3a720013cc24a847765ffcad86140db
SiarheiZhachek <SiarheiZhachek@yandex.ru>
2022-12-09 21:36:42 +0300

    after_delete_this_task

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,11 +1,8 @@
 from selenium.webdriver.common.by import By
-from time import sleep
 
 
 def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
     driver.get(link)
     login_link = driver.find_element(By.CSS_SELECTOR, "#login_link")
-    sleep(3)
     login_link.click()
-
--- tests/test_23.py
+++ tests/test_23.py
@@ -1,12 +1,13 @@
 from selenium.webdriver.common.by import By
-from selenium import webdriver
-from time import sleep
 
 
 def test_1(driver):
-    driver.get('http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear')
+    driver.get(
+        'http://selenium1py.pythonanywhere.com/'
+        'catalogue/the-shellcoders-handbook_209/?promo=newYear')
     title_1 = driver.find_element(By.CSS_SELECTOR, '.col-sm-6.product_main h1')
     print(title_1.text)
-    price_1 = driver.find_element(By.CSS_SELECTOR, '.col-sm-6.product_main .price_color')
+    price_1 = driver.find_element(
+        By.CSS_SELECTOR, '.col-sm-6.product_main .price_color'
+    )
     print(price_1.text)
-
